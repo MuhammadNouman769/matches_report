680796d87e0455092ac5b8d5683ba20e182bcb9b
Muhammad Nouman <nomannisar769@gmail.com>
2026-01-30 18:22:12 +0500

    metadata

--- apps/stories/models.py
+++ apps/stories/models.py
@@ -19,12 +19,27 @@ class Story(models.Model):
         CANCEL = 'cancel', 'Cancel'
 
 
-    '''core fields'''
+    ''' ------- core fields -------- '''
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
     content = models.TextField()
     image = models.ImageField(upload_to='stories/%Y/%m/%d', blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stories')
     status = models.CharField(max_length=10, choices=StoryStatus.choices,default=StoryStatus.Draft)
+    tags = models.ForeignKey()
     
+    '''Meta Data'''
+    summery = models.TextField(blank=True, help_text="Brief summery of story")
+    published_at = models.DateTimeField(null=True, blank=True)
+
+    '''Review work flow'''
+    reviewed_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,related_name='rewieved_stories'
+    )
+    reviewed_at = models.TextField(blank=True, help_text="Review comments from chief editor")
+
+    ''' '''
 
